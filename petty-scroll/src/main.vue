7667a883657cdf53c24748a92d270bd837a34b5e
Pettyfer <pettyferlove@live.cn>
2017-09-24 16:25:43 +0800

    commit

--- petty-scroll/src/main.vue
+++ petty-scroll/src/main.vue
@@ -31,15 +31,15 @@
           moveY: 0,
           preScrollHeigt: 0,
           opt: {
-            barColor: '#787878',   // 滚动条颜色
-            barWidth: 6,           // 滚动条宽度
-            railColor: '#eee',     // 导轨颜色
-            barMarginRight: 0,     // 垂直滚动条距离整个容器右侧距离单位（px）
-            barMaginBottom: 0,     // 水平滚动条距离底部距离单位（px)
-            barOpacityMin: 0.5,      // 滚动条非激活状态下的透明度
-            zIndex: 'auto',        // 滚动条z-Index
-            autohidemode: true,     // 自动隐藏模式
-            horizrailenabled: true // 是否显示水平滚动条
+            barColor: '#787878',
+            barWidth: 6,
+            railColor: '#eee',
+            barMarginRight: 0,
+            barMaginBottom: 0,
+            barOpacityMin: 0.5,
+            zIndex: 'auto',
+            autohidemode: true,
+            horizrailenabled: true
           }
         }
       },
