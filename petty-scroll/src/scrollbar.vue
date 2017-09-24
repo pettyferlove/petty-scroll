1855b805a7e7a1676b392c3f99ce2678a1792cf4
Pettyfer <pettyferlove@live.cn>
2017-09-24 14:00:49 +0800

    commit

--- petty-scroll/src/scrollbar.vue
+++ petty-scroll/src/scrollbar.vue
@@ -1,4 +1,6 @@
 <script>
+  import SCROLLBAR_MAP from './utils/scrollbar-V&H'
+
   export default {
     name: 'ScrollBar',
     /**
@@ -12,8 +14,21 @@
       barStyle: Object,
       railStyle: Object
     },
+    computed: {
+      bar: function () {
+        return SCROLLBAR_MAP[this.vertical ? 'vertical' : 'horizontal']
+      },
+      railStyles () {
+        return this.vertical ? 'background-color:' + this.railStyle.color +
+          ';z-Index:' + this.railStyle.zIndex +
+          ';width:' + this.barStyle.width +
+          ';right:' + this.railStyle.marginRight : 'background-color:' + this.railStyle.color +
+          ';z-Index:' + this.railStyle.zIndex +
+          ';height:' + this.barStyle.width +
+          ';bottom:' + this.railStyle.marginBottom
+      }
+    },
     render: function (h) {
-      console.log(this.vertical)
       let wrap = this.$slots.default
       let railbar = h('div', {
         attrs: {
@@ -21,16 +36,11 @@
         },
         ref: 'railbar',
         class: {
-          'petty-scrollbar-bar': true
+          'petty-scrollbar-bar': true,
+          'is-horizontal': !this.vertical,
+          'is-vertical': this.vertical
         },
-        style: {
-          backgroundColor: this.railStyle.color,
-          zIndex: this.railStyle.zIndex,
-          width: this.barStyle.width,
-          right: this.railStyle.marginRight,
-          height: this.barStyle.width,
-          bottom: this.railStyle.marginBottom
-        }
+        style: this.railStyles
       })
       let nodes = ([
         wrap,
